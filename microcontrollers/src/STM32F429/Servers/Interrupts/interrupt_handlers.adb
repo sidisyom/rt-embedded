6f51d1a0f54e71952b99804e3635a3f6ed19c767
j-mbd <34354874+j-mbd@users.noreply.github.com>
2025-03-04 16:54:28 +0000

    EXTI4 interrupt handler implementation

--- microcontrollers/src/STM32F429/Servers/Interrupts/interrupt_handlers.adb
+++ microcontrollers/src/STM32F429/Servers/Interrupts/interrupt_handlers.adb
@@ -1,5 +1,7 @@
 with Common_Values;
 with ADC;
+with EXTI;
+with Common_Types;
 
 package body Interrupt_Handlers is
 
@@ -9,4 +11,25 @@ package body Interrupt_Handlers is
          Common_Values.Measured_ADC_Value := ADC.ADC1_DR_Reg;
       end Handle;
    end ADC_Interrupt_Handler;
+
+   protected body EXTI4_Interrupt_Handler is
+      procedure Handle_Interrupt is
+         use Ada.Real_Time;
+      begin
+         EXTI.EXTI_IMR_Reg.MR4 := Common_Types.Off; -- disable interrupt
+         EXTI.EXTI_PR_Reg.PR4 := Common_Types.On; -- clear pending status (bit needs to be set **HIGH** in order to do that)
+         Ada.Real_Time.Timing_Events.Set_Handler (Event, Ada.Real_Time.Clock + MIT.all, Handle_Timeout'Access); -- setup interrupt-enable event to fire after MIT
+         Arrived := True;
+      end Handle_Interrupt;
+
+      entry Wait_For_Next_Interrupt when Arrived is
+      begin
+         Arrived := False;
+      end Wait_For_Next_Interrupt;
+
+      procedure Handle_Timeout (E : in out Ada.Real_Time.Timing_Events.Timing_Event) is
+      begin
+         EXTI.EXTI_IMR_Reg.MR4 := Common_Types.On; -- enable the interrupt
+      end Handle_Timeout;
+   end EXTI4_Interrupt_Handler;
 end Interrupt_Handlers;
--- microcontrollers/src/STM32F429/Servers/Interrupts/interrupt_handlers.ads
+++ microcontrollers/src/STM32F429/Servers/Interrupts/interrupt_handlers.ads
@@ -1,12 +1,31 @@
 with Ada.Interrupts.Names;
 with System;
+with Ada.Real_Time;
+with Ada.Real_Time.Timing_Events;
 
 package Interrupt_Handlers with Elaborate_Body is
-
+   --   ADC
    protected ADC_Interrupt_Handler with
      Interrupt_Priority => System.Max_Interrupt_Priority
    is
       procedure Handle with
         Attach_Handler => Ada.Interrupts.Names.ADC_Interrupt;
    end ADC_Interrupt_Handler;
+
+   --   EXTI4
+   protected type EXTI4_Interrupt_Handler (MIT : access constant Ada.Real_Time.Time_Span) with
+     Interrupt_Priority => System.Max_Interrupt_Priority
+   is
+      entry Wait_For_Next_Interrupt;
+   private
+      procedure Handle_Interrupt with
+        Attach_Handler => Ada.Interrupts.Names.EXTI4_Interrupt;
+      procedure Handle_Timeout (E : in out Ada.Real_Time.Timing_Events.Timing_Event);
+      Event : Ada.Real_Time.Timing_Events.Timing_Event;
+      Arrived : Boolean;
+   end EXTI4_Interrupt_Handler;
+
+   EXTI4_MIT : aliased Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds (1_00);
+   EIH : EXTI4_Interrupt_Handler (MIT => EXTI4_MIT'Access);
+
 end Interrupt_Handlers;
