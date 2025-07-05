3e6bd836915037faf052f046a2bd775c40159c7a
Savvas Moysidis <s.moysidis@fastmail.com>
2025-07-05 21:01:52 +0100

    Test refactoring works for gpio package

--- microcontrollers/src/microcontrollers.adb
+++ microcontrollers/src/microcontrollers.adb
@@ -1,22 +1,19 @@
+with Hal.Stm32;
 with Hal.Stm32.Stm32f429;
-with Registers.Stm32f4;
-
-with System.BB.CPU_Primitives;
 
 procedure Microcontrollers is
 begin
    declare
-      use Registers.Stm32f4;
-      Request : constant Hal.Stm32.GPIO_Configuration_Request := (Port => Registers.Stm32f4.G,
+      use Hal.Stm32;
+      Request : constant Hal.Stm32.GPIO_Configuration_Request := (Port => G,
                                                                   Pin  => 13,
-                                                                  Mode => Registers.Stm32f4.Output);
-      Current_State : Registers.Stm32f4.Binary_State := Registers.Stm32f4.On;
+                                                                  Mode => Output);
+      Current_State : Binary_State := On;
    begin
       Hal.Stm32.Stm32f429.Configure_Gpio (Request);
       loop
          Hal.Stm32.Stm32f429.Set_Pin_State (Port => Request.Port, Pin => Request.Pin, State => Current_State);
-         Current_State := (if Current_State = Registers.Stm32f4.On then Registers.Stm32f4.Off else Registers.Stm32f4.On);
-
+         Current_State := (if Current_State = On then Off else On);
          delay 2.0;
       end loop;
    end;
